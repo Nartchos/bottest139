af2f31a05503957b6130ac4527f95bec02661ff3
Nartchos <40717967+Nartchos@users.noreply.github.com>
2018-08-23 18:37:23 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -30,4 +30,19 @@ bot.on('message', message => {
         message.channel.sendMessage("La team PikaWolf est une team créer par Laval ! Au début, il y avait que Water_Fire_YTB qui s'occuper des recrutements ! Maintenant, nous sommes beaucoups plus :)");
     }
 
-});
+    if (message.content.startsWith(prefix + "sondage")) {
+        let args = message.content.split(" ").slice(1);
+        let thingToEcho = args.join(" ")
+        var embed = new Discord.RichEmbed()
+            .setDescription("Sondage")
+            .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
+            .setColor("0xB40404")
+        message.guild.channels.find("name", "sondage").sendEmbed(embed)
+        .then(function (message) {
+            message.react("√")
+            message.react("✖")
+        }).catch(function() {
+        });
+        }else{
+            return message.reply("Tu n'as pas la permission.")
+}}})
